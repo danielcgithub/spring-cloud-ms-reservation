f8e12b797be6023b1d7d713821f467b4044d0edd
danielcgithub <d.cunniffe.ie@gmail.com>
2019-09-03 16:36:53 +0100

    Building basic reservation service

--- reservation-service/src/main/java/com/danielcgithub/reservationservice/ReservationServiceApplication.java
+++ reservation-service/src/main/java/com/danielcgithub/reservationservice/ReservationServiceApplication.java
@@ -1,13 +1,80 @@
 package com.danielcgithub.reservationservice;
 
+import java.util.Collection;
+import java.util.stream.Stream;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 @SpringBootApplication
 public class ReservationServiceApplication {
 
+	@Bean
+	CommandLineRunner commandLineRunner(ReservationRepository reservationRepository) {
+		return strings -> {
+			Stream.of("Josh", "Pieter", "Tasha", "Eric", "Susie", "Max")
+					.forEach(n -> reservationRepository.save(new Reservation(n)));
+		};
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(ReservationServiceApplication.class, args);
+	}
+
+}
+
+// object that handles the boiler plate of CRUD
+@RepositoryRestResource
+interface ReservationRepository extends JpaRepository<Reservation, Long> {
+	@RestResource(path = "by-name")
+	Collection<Reservation> findByReservationName(@Param("rn") String rn);
+}
+
+@Entity
+class Reservation {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String reservationName;
+
+	public Reservation() {// for JPA
+
+	}
+
+	public Reservation(String n) {
+		this.reservationName = n;
+	}
+
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getReservationName() {
+		return this.reservationName;
+	}
+
+	public void setReservationName(String reservationName) {
+		this.reservationName = reservationName;
+	}
+
+	@Override
+	public String toString() {
+		return "{" + " id='" + id + "'" + ", reservationName='" + reservationName + "'" + "}";
 	}
 
 }
