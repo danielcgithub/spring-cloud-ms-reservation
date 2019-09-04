1e8ea6f44b80ef67d2a13883490bc2a2cf4711ab
danielcgithub <d.cunniffe.ie@gmail.com>
2019-09-04 12:22:44 +0100

    Updating reservation service to read from config server including adding message endpoint

--- reservation-service/src/main/java/com/danielcgithub/reservationservice/ReservationServiceApplication.java
+++ reservation-service/src/main/java/com/danielcgithub/reservationservice/ReservationServiceApplication.java
@@ -7,14 +7,18 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ReservationServiceApplication {
@@ -32,6 +36,19 @@ public class ReservationServiceApplication {
 		SpringApplication.run(ReservationServiceApplication.class, args);
 	}
 
+}
+
+@RefreshScope
+@RestController
+class MessageRestController {
+
+	@Value("${message}")
+	private String msg;
+
+	@RequestMapping("/message")
+	String message() {
+		return this.msg;
+	}
 }
 
 // object that handles the boiler plate of CRUD
